10d847ebfc789a484fbd9d25e861863d4bcd7f31
Blackspider2003 <mdbishalahmedrafi55@gmail.com>
2025-08-12 00:10:20 +0600

    Work with Featured Product Show

--- src/components/NavbarIcons.tsx
+++ src/components/NavbarIcons.tsx
@@ -44,13 +44,13 @@ const NavbarIcons = () => {
         height={22}
         className="cursor-pointer"
       ></Image>
-      <div className="relative cursor-pointer ">
+      <div className="relative cursor-pointer" onClick={() => setIsCartOpen((prev) => !prev)}>
       <Image
         src={"/cart.png"}
         alt="profile Image"
         width={22}
         height={22}
-        onClick={() => setIsCartOpen((prev) => !prev)} 
+        
       ></Image>
       <div className="absolute -top-4 -right-4 h-6 bg-primary rounded-full text-white text-sm flex items-center justify-center">
       2
